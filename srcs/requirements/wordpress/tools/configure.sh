7e9f9e49bbb4db4950838c493a68e2403e11942d
vaghazar <vaghazar@debian.42yerevan.am>
2023-09-01 06:13:39 -0400

    finished

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -1,43 +1,19 @@
-# #!/bin/bash
+#!/bin/bash
 
-# CERTIFICATE_PEM=/etc/nginx/ssl/vaghazar.pem
-# CERTIFICATE_KEY=/etc/nginx/ssl/vaghazar.key
-# DOMAIN_NAME=vaghazar.42.fr
-# WORDPRESS_HOSTNAME=wordpress
-# MYSQL_HOSTNAME=mariadb
-# MYSQL_DATABASE=wordpress
-# MYSQL_USER=vaghazar
-# MYSQL_PASSWORD=1234
-# MYSQL_ROOT_PASSWORD=1234
-# WP_TITLE=inception
-# WP_ADMIN_NAME=vaghazar
-# WP_ADMIN_PASSWORD=vaghazar
-# WP_ADMIN_EMAIL=vahan.ghazaryan.00@mail.ru
-# WP_USER_NAME=user
-# WP_USER_PASSWORD=user
-# WP_USER_EMAIL=vahan.ghazaryan.00@gmail.com
+wp_config=./wp-config.php;
 
-# wp_config=./wp-config.php;
+sleep 1;
 
-# sleep 1;
+wp core install --allow-root --path=./ \
+	--url=$DOMAIN_NAME \
+	--title=$WP_TITLE \
+	--admin_user=$WP_ADMIN_NAME \
+	--admin_password=$WP_ADMIN_PASSWORD \
+	--admin_email=$WP_ADMIN_EMAIL;
 
-# sed -i "s/MYSQL_DATABASE/$MYSQL_DATABASE/g"	$wp_config;
-# sed -i "s/MYSQL_USER/$MYSQL_USER/g"  	  	$wp_config;
-# sed -i "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g" 	$wp_config;
-# sed -i "s/MYSQL_HOSTNAME/$MYSQL_HOSTNAME/g" 	$wp_config;
+wp user create --allow-root --path=./ \
+	$WP_USER_NAME \
+	$WP_USER_EMAIL \
+	--user_pass=$WP_USER_PASSWORD;
 
-# wp core install --allow-root --path=./ \
-# 	--url=$DOMAIN_NAME \
-# 	--title=$WP_TITLE \
-# 	--admin_user=$WP_ADMIN_NAME \
-# 	--admin_password=$WP_ADMIN_PASSWORD \
-# 	--admin_email=$WP_ADMIN_EMAIL;
-
-# wp user create --allow-root --path=./ \
-# 	$WP_USER_NAME \
-# 	$WP_USER_EMAIL \
-# 	--user_pass=$WP_USER_PASSWORD;
-
-# wp theme activate twentytwentytwo --allow-root --path=./;
-
-# exec "$@";
+exec "$@";
